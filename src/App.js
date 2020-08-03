b4a94f0c58640c21e411152c083ec384e2af5a4f
Linda <msh1730@hanmail.net>
2020-08-03 17:57:54 +0900

    useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-//함수형에서 props받으려면
-const App = (props) => {
-  const [value, setValue] = useState({
-    id: "ssar",
-    password: "1234",
-  }); //초기값 0자리에 오브젝트 들어갈 수 있음.
+const App = () => {
+  const [name, setName] = useState("홍길동");
+  const [nickname, setNickname] = useState("무적");
 
-  const [user, setUser] = useState({
-    username: "ssarmango",
-    email: "ssarmango",
-  });
+  //특정 값이 변경될 때 캐치하고 싶음. 여러개도 가능
+  useEffect(() => {
+    console.log("렌더링이 완료되었습니다.");
+  }, [name]); //인자 함수 딴거모르겠고 name처리된 거 만보고 내가 처리할께
 
-  let change = () => {
-    setValue({ ...value, password: "5678" }); //초기화했던 타입을 넣으면 됨.
+  const changeName = () => {
+    setName("김병만");
+  };
+
+  const changeNickname = () => {
+    setNickname("약함");
   };
 
   return (
     <div>
-      <h1>{value.id}</h1>
-      <h1>{value.password}</h1>
-      <h1>{user.username}</h1>
-      <button onClick={change}>수정</button>
+      <div>{name}</div>
+      <div>{nickname}</div>
+      <button onClick={changeName}>이름변경</button>
+      <button onClick={changeNickname}>별명변경</button>
     </div>
   );
 };
