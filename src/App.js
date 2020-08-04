841606281c653ce2e0247c2c15940efabb803945
Linda <msh1730@hanmail.net>
2020-08-04 17:48:22 +0900

    useRef

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
-import React, { useState, useMemo } from "react";
-
-const getAverage = (numbers) => {
-  console.log("평균값 계산중...");
-  if (numbers.length === 0) return 0;
-  const sum = numbers.reduce((a, b) => a + b); //a는 누적값
-  console.log(sum);
-  return sum / numbers.length;
-};
+import React, { useEffect, useRef } from "react";
 
 const App = () => {
-  const [list, setList] = useState([]);
-  const [number, setNumber] = useState("");
+  const input1 = useRef(null);
 
-  const onChange = (e) => {
-    setNumber(e.target.value);
-  };
-
-  const onInsert = (e) => {
-    //등록버튼 누르면
-    const nextList = list.concat(parseInt(number));
-    setList(nextList);
-    setNumber("");
-  };
+  //didmount
+  useEffect(() => {
+    console.log(input1);
+    input1.current.focus();
+  }, []);
 
   return (
     <div>
-      <input value={number} onChange={onChange} />
-      <button onClick={onInsert}>등록</button>
-      <ul>
-        {list.map((value, index) => (
-          <li key={index}>{value}</li>
-        ))}
-      </ul>
-      <div>
-        <b>평균값:</b> {getAverage(list)}
-      </div>
+      <input type="text" ref={input1} />
     </div>
   );
 };
